da17e8ad2c0adb96038cafbb5982d042d7531c5a
Adam Durham <amdnative@gmail.com>
2023-12-19 22:05:09 -0500

    ...

--- terraform/active-directory.tf
+++ terraform/active-directory.tf
@@ -1,4 +1,4 @@
-resource "nsxt_policy_fixed_segment" "active-directory" {
+resource "nsxt_policy_fixed_segment" "active_directory" {
   display_name      = "Homelab - Active Directory"
   description       = "Terraform provisioned Segment"
   connectivity_path = nsxt_policy_tier1_gateway.tier1_gw.path
@@ -13,16 +13,71 @@ resource "nsxt_policy_fixed_segment" "active-directory" {
   }
 }
 
-module "homelab_active-directory" {
+resource "vsphere_folder" "active_directory" {
+  path          = "Active Directory"
+  type          = "vm"
+  datacenter_id = vsphere_datacenter.Homelab.moid
+}
+
+module "homelab-active_directory" {
   source    = "git@github.com:adurham/terraform-vsphere-vm.git?ref=v3.8.1"
   vmtemp    = "Windows Server 2022 Datacenter"
-  vmfolder = "Active Directory"
+  vmfolder = vsphere_folder.active_directory.path
   instances = 3
   cpu_number = 2
+  ram_size = 4096
   vmname    = "amd-wnad"
-  vmrp      = "amd-vmcl02/Resources"
+  vmrp      = "${vsphere_compute_cluster.cl02.name}/Resources"
   network = {
-    "Homelab - Active Directory" = ["172.16.0.3", "172.16.0.4", "172.16.0.5"]
+    "${nsxt_policy_fixed_segment.active_directory.display_name}" = ["172.16.0.3", "172.16.0.4", "172.16.0.5"]
+  }
+  vmgateway = "172.16.0.1"
+  dc        = "Homelab"
+  datastore = "vsanDatastore"
+  is_windows_image = true
+}
+
+resource "vsphere_folder" "active_directory_federation_services" {
+  path          = "ADFS"
+  type          = "vm"
+  datacenter_id = vsphere_datacenter.Homelab.moid
+}
+
+module "homelab-active_directory_federation_services" {
+  source    = "git@github.com:adurham/terraform-vsphere-vm.git?ref=v3.8.1"
+  vmtemp    = "Windows Server 2022 Datacenter"
+  vmfolder = vsphere_folder.active_directory_federation_services.path
+  instances = 3
+  cpu_number = 2
+  ram_size = 4096
+  vmname    = "amd-wnadfs"
+  vmrp      = "${vsphere_compute_cluster.cl02.name}/Resources"
+  network = {
+    "${nsxt_policy_fixed_segment.active_directory.display_name}" = ["172.16.0.7", "172.16.0.8", "172.16.0.9"]
+  }
+  vmgateway = "172.16.0.1"
+  dc        = "Homelab"
+  datastore = "vsanDatastore"
+  is_windows_image = true
+}
+
+resource "vsphere_folder" "active_directory_certificate_authority" {
+  path          = "ADCA"
+  type          = "vm"
+  datacenter_id = vsphere_datacenter.Homelab.moid
+}
+
+module "homelab-active_directory_certificate_authority" {
+  source    = "git@github.com:adurham/terraform-vsphere-vm.git?ref=v3.8.1"
+  vmtemp    = "Windows Server 2022 Datacenter"
+  vmfolder = vsphere_folder.active_directory_certificate_authority.path
+  instances = 3
+  cpu_number = 2
+  ram_size = 4096
+  vmname    = "amd-wnadca"
+  vmrp      = "${vsphere_compute_cluster.cl02.name}/Resources"
+  network = {
+    "${nsxt_policy_fixed_segment.active_directory.display_name}" = ["172.16.0.9", "172.16.0.10", "172.16.0.11"]
   }
   vmgateway = "172.16.0.1"
   dc        = "Homelab"
---
+++ terraform/vault.tf
@@ -0,0 +1,42 @@
+resource "nsxt_policy_fixed_segment" "vault" {
+  display_name      = "Homelab - Vault"
+  description       = "Terraform provisioned Segment"
+  connectivity_path = nsxt_policy_tier1_gateway.tier1_gw.path
+  subnet {
+    cidr        = "172.16.0.193/26"
+    dhcp_v4_config {
+        dns_servers = [
+            "10.0.3.129"
+        ]
+        lease_time = 86400
+    }
+  }
+}
+
+resource "vsphere_folder" "vault" {
+  path          = "Vault"
+  type          = "vm"
+  datacenter_id = vsphere_datacenter.Homelab.moid
+}
+
+module "homelab-vault_server" {
+  depends_on = [
+    nsxt_policy_fixed_segment.vault,
+    vsphere_folder.vault
+  ]
+  source    = "git@github.com:adurham/terraform-vsphere-vm.git?ref=v3.8.1"
+  vmtemp    = "Ubuntu Linux 22.04"
+  vmfolder = vsphere_folder.vault.path
+  instances = 3
+  cpu_number = 2
+  ram_size = 4096
+  vmname    = "amd-lxvlt"
+  vmrp      = "${vsphere_compute_cluster.cl02.name}/Resources"
+  domain = "lab.amd-e.com"
+  network = {
+    "${nsxt_policy_fixed_segment.vault.display_name}" = ["172.16.0.195", "172.16.0.196", "172.16.0.197"]
+  }
+  vmgateway = "172.16.0.193"
+  dc        = "${vsphere_datacenter.Homelab.name}"
+  datastore = "vsanDatastore"
+}
