ca8c2869635a3169e3572ce2eeb2d5a2e7c34d51
Adam Durham <adam.durham@tanium.com>
2024-01-05 17:35:43 -0500

    ...

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,16 +1,16 @@
-# terraform {
-#   backend "local" {
-#     path = "terraform.tfstate"
-#   }
-# }
-
 terraform {
-  backend "consul" {
-    address = "172.16.0.131:8500"
-    scheme  = "http"
-    path    = "terraform/homelab"
-    # access_token = ""
-    datacenter = "homelab"
-    gzip = true
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
+
+# terraform {
+#   backend "consul" {
+#     address = "172.16.0.131:8500"
+#     scheme  = "http"
+#     path    = "terraform/homelab"
+#     # access_token = ""
+#     datacenter = "homelab"
+#     gzip = true
+#   }
+# }
